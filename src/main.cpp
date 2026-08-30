e0ae2106334fb828f127ed73f872273e68529fdf
Stasiks <stanislavvv2011@gmail.com>
2026-08-30 23:37:15 +0300

    Implement clicker and IMU modes with display updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
 #include <M5StickCPlus.h>
+int currentMode = 0; // 0 — режим кликера, 1 — режим акселерометра
+float accX = 0.0F;
+float accY = 0.0F;
+float accZ = 0.0F;
 int counter = 0;
+
+void runClicker() {
+    if (M5.BtnA.wasPressed()) {
+        counter++;
+        M5.Lcd.fillScreen(BLACK);
+        M5.Lcd.setCursor(10, 30);
+        M5.Lcd.print(counter);
+    }
+    if (M5.BtnB.wasPressed()) {
+        counter = max(counter - 1, 0);
+        M5.Lcd.fillScreen(BLACK);
+        M5.Lcd.setCursor(10, 30);
+        M5.Lcd.print(counter);
+    }
+}
+
+void runImu() {
+    M5.IMU.getAccelData(&accX, &accY, &accZ);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(10, 20);
+    M5.Lcd.printf("X: %.2f\nY: %.2f\nZ: %.2f", accX, accY, accZ);
+    delay(100);
+}
 
 void setup() {
     M5.begin();                // 1. Включаем плату и шины питания
@@ -11,6 +38,7 @@ void setup() {
     M5.Lcd.setCursor(10, 30);    // Координаты X, Y в пикселях
 
     M5.Lcd.println("Hello, Stas!");
+    M5.IMU.Init(); // Запуск чипа датчика
 }
 void updateDisplay() {
     M5.Lcd.fillScreen(BLACK);
@@ -21,13 +49,13 @@ void updateDisplay() {
 }
 void loop() {
     M5.update(); // Опрос кнопок
-    if (M5.BtnA.wasPressed()) {
-    counter = counter + 1; // Увеличиваем на 1
-    // Твои команды очистки экрана и вывода counter
-    updateDisplay();
-    } else if (M5.BtnB.wasPressed()) {
-    counter = counter - 1;
-    counter = max(counter, 0);
-     updateDisplay();
+    if (M5.BtnB.wasReleasefor(700)) {
+      currentMode = 1 - currentMode;
+      M5.Lcd.fillScreen(BLACK);
     }
-}
+    if (currentMode == 0) {
+        runClicker();
+    } else {
+        runImu();
+    }
+  }
