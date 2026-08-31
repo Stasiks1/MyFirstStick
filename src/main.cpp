faa1c51761d09ed8a66350c0182530c4e2619ad9
Stasiks <stanislavvv2011@gmail.com>
2026-08-31 18:12:51 +0300

    Add stopwatch functionality with start/pause feature and display updates

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@ float accX = 0.0F;
 float accY = 0.0F;
 float accZ = 0.0F;
 int counter = 0;
+int seconds = 0;
+bool isRunning = false;
+unsigned long previousMillis = 0;
 
 void runClicker() {
     if (M5.BtnA.wasPressed()) {
@@ -28,13 +31,38 @@ void runImu() {
     delay(100);
 }
 
+void runStopwatch() {
+    if (M5.BtnA.wasPressed()) {
+    isRunning = !isRunning;
+     M5.Lcd.setCursor(20, 100);
+    if (isRunning) {
+        M5.Beep.tone(4000, 50);
+        M5.Lcd.print("RUN  ");   // Пробелы, чтобы затереть слово PAUSE
+    } else {
+        M5.Beep.tone(4000, 50);
+        M5.Lcd.print("PAUSE");
+    }
+}
+    if (isRunning && M5.BtnB.wasPressed()) {
+        seconds = 0;
+        M5.Lcd.setCursor(80,50);
+        M5.Lcd.printf("%02d", seconds);
+    }
+    if (isRunning && (millis() - previousMillis >= 1000)) {
+        previousMillis = millis();
+        seconds++;
+        M5.Lcd.setCursor(80, 50);
+        M5.Lcd.printf("%02d", seconds);
+    }
+}
+
 void setup() {
     M5.begin();                // 1. Включаем плату и шины питания
     M5.Lcd.setRotation(3);     // 2. Поворачиваем экран горизонтально (0-3)
     M5.Lcd.fillScreen(BLACK);  // 3. Заливаем экран черным цветом
 
     M5.Lcd.setTextDatum(MC_DATUM);
-    M5.Lcd.setTextColor(YELLOW); // Цвет текста
+    M5.Lcd.setTextColor(YELLOW, BLACK); // Цвет текста и фона
     M5.Lcd.setTextSize(3);       // Размер шрифта
 
     M5.Lcd.drawString("Hello", 120, 67);
@@ -48,17 +76,39 @@ void updateDisplay() {
     M5.Lcd.print(counter);
 }
 void loop() {
+    M5.Beep.update();
     M5.update(); // Опрос кнопок
+    M5.Beep.update();
     if (M5.BtnB.wasReleasefor(700)) {
-      currentMode = 1 - currentMode;
-      M5.Lcd.fillScreen(BLACK);
-      if (currentMode == 0) {
+        currentMode++;
+        if (currentMode > 2) {
+            currentMode = 0;
+        }
+        M5.Lcd.fillScreen(BLACK);
+        if(currentMode == 0) {
             updateDisplay();
+        }else if (currentMode == 2) {
+             // Начальный экран секундомера:
+            M5.Lcd.setCursor(20, 20);
+             M5.Lcd.setTextSize(2);
+            M5.Lcd.print("STOPWATCH");
+    
+            M5.Lcd.setCursor(80, 50);
+            M5.Lcd.printf("%02d", seconds);
+
+            M5.Lcd.setCursor(20, 100);
+            M5.Lcd.print("PAUSE");
         }
     }
-    if (currentMode == 0) {
+        switch (currentMode) {
+    case 0:
         runClicker();
-    } else {
+        break;
+    case 1:
         runImu();
+        break;
+    case 2:
+        runStopwatch();
+        break;
+}
     }
-  }
